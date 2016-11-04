2bb2bd5d1c3c4fdd4c19969a4d3ba74ac7f48391
Patrick Riley <priley86@gmail.com>
2016-11-04 18:54:32 -0400

    downstream wc-test

--- pages/environments/index.js
+++ pages/environments/index.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
-import Layout from '../../components/Layout';
-import WizardView from '../../components/Wizard/WizardView';
-import c from '../common.css';
+import PfAlert from 'patternfly-webcomponents/dist/es2015/pf-alert/index.js'
+// import { PfAlert } from 'patternfly-webcomponents';
 
 class EnvironmentsPage extends React.Component {
 
@@ -11,41 +10,12 @@ class EnvironmentsPage extends React.Component {
     document.title = 'Patternfly React Boiler | Environments';
   }
 
-  handleClick = (event) => {
-    this.setState({wizardView: true});
-  };
-
-  handleClose = (event) => {
-    this.setState({wizardView: false});
-  };
-
   render() {
-    const { projects } = this.props;
-
-    if(this.state.wizardView){
-      return (
-        <Layout className={c.add_layout}>
-          <div className={c.add_container + ' container-pf-nav-pf-vertical'}>
-            <div className={c.add_button} onClick={this.handleClick}>
-              <i className="fa fa-4x fa-plus-circle" aria-hidden="true"></i>
-              <h3>Add environments</h3>
-            </div>
-          </div>
-          <WizardView handleClose={this.handleClose.bind(this)}/>
-        </Layout>
-      );
-    } else {
-      return (
-        <Layout className={c.add_layout}>
-          <div className={c.add_container + ' container-pf-nav-pf-vertical'}>
-            <div className={c.add_button} onClick={this.handleClick}>
-              <i className="fa fa-4x fa-plus-circle" aria-hidden="true"></i>
-              <h3>Add environments</h3>
-            </div>
-          </div>
-        </Layout>
-      );
-    }
+    return (
+    <pf-alert type="danger">
+      <strong>Hey there is a problem!</strong> Yeah this is really messed up and you should <a href="#" class="alert-link">know about it</a>.
+    </pf-alert>
+    )
   }
 
 }
